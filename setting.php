c38496a1fb1457c4cf47eb64a5a50c76c790bc79
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-10-19 13:05:37 +0800

    zj~

---
+++ setting.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST["prob"])){
+	$content=$_POST["prob"];
+	file_put_contents("prob.txt",$content);
+}
+if(isset($_POST["user"])){
+	$content=$_POST["user"];
+	$content=str_replace(" ","\t",$content);
+	file_put_contents("user.txt",$content);
+}
+?>
+<form method="POST">
+<textarea name="prob" cols="20" rows="30">
+<?=htmlentities(@file_get_contents("prob.txt"))?>
+</textarea>
+<textarea name="user" cols="40" rows="30">
+<?=htmlentities(@file_get_contents("user.txt"))?>
+</textarea>
+<br>
+<input type="submit" value="Submit">
+</form>
